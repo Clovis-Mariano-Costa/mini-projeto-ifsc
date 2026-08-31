b964f7de513e7e32e6f5f4631683a451b3e9a12f
Clovis Mariano da Costa <aeonprimevo@gmail.com>
2026-08-30 21:09:04 -0300

    skillmatch - inicializando
    inicializa perfil do candidato

---
+++ skillmatch.js
@@ -0,0 +1,25 @@
+/*
+ * SkillMatch JS
+ * Mini-Projeto Avaliativo - Módulo 01
+ * Programação Front-End React - IFSC
+ *
+ * Este arquivo contém a lógica principal do projeto SkillMatch JS.
+ *
+ * O objetivo do sistema é comparar as habilidades de uma pessoa candidata
+ * com os requisitos de vagas fictícias para Front-End Júnior.
+ *
+ * O programa deverá analisar:
+ * - habilidades que o candidato possui;
+ * - requisitos exigidos pelas vagas;
+ * - habilidades faltantes;
+ * - percentual de compatibilidade;
+ * - classificação da compatibilidade;
+ * - vaga com maior compatibilidade;
+ * - recomendação de estudo.
+ *
+ * Autor: Clovis Mariano da Costa
+ * Status: Em desenvolvimento
+ */
+// ==============================
+// PERFIL DO CANDIDATO
+// ==============================
